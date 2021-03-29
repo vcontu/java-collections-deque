3e802e7a5dc50cfe2e96f343d91b740ceb4cd641
Viorel Contu <Viorel.Contu@endava.com>
2021-03-29 21:55:28 +0300

    initial commit for Java Collections Deque Homework

---
+++ src/main/java/com/endava/internship/collections/StudentDeque.java
@@ -0,0 +1,210 @@
+package com.endava.internship.collections;
+
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+
+public class StudentDeque implements Deque<Student> {
+    @Override
+    public void addFirst(Student student) {
+        //TODO
+    }
+
+    @Override
+    public void addLast(Student student) {
+        //TODO
+    }
+
+    @Override
+    public boolean offerFirst(Student student) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public boolean offerLast(Student student) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public Student removeFirst() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Student removeLast() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Student pollFirst() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Student pollLast() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Student getFirst() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Student getLast() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Student peekFirst() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Student peekLast() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public boolean removeFirstOccurrence(Object o) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public boolean removeLastOccurrence(Object o) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public boolean add(Student student) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public boolean offer(Student student) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public Student remove() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Student poll() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Student element() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Student peek() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void push(Student student) {
+        //TODO
+    }
+
+    @Override
+    public Student pop() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public int size() {
+        //TODO
+        return 0;
+    }
+
+    @Override
+    public Iterator<Student> iterator() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public Iterator<Student> descendingIterator() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public Object[] toArray() {
+        //TODO
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] ts) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void clear() {
+        //TODO
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Student> collection) {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> collection) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> collection) {
+        throw new UnsupportedOperationException();
+    }
+}
